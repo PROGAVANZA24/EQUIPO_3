2c8ef46a44360ac8937cd1ec8919bb3ae4c49f00
Eduardo Missael Torres Lopez <torrese620@gmail.com>
2021-05-19 18:43:34 -0500

    Se creo la clase Clase_tema_video.py

---
+++ clase_tema_video.py
@@ -0,0 +1,33 @@
+class Curso_Tema_Video:
+    def __init__(self, id_CTV, id_CT, id_video):
+     self.__id_CTV = id_CTV
+     self.__id_CT = id_CT
+     self.__id_video = id_video
+
+    def guardar(self,id_CTV, id_CT, id_video):
+        lista = [id_CTV,"|", id_CT,"|", id_video]
+        f = open("c:/Archivos/Clonados/EQUIPO_3/clase_tema_video.txt","a",enconding="utf8")
+        for numero in lista:
+             f.write(str(numero)+'\n')
+        f.close
+
+    def consultar_todo(self):
+        f = open("c:/Archivos/Clonados/EQUIPO_3/clase_tema_video.txt",encoding="utf8")
+
+        print(f"{'idCTV':<10}{'idCT':>10}{'idVIDEO':>10}")
+        for linea in f:
+            datos = linea.strip().split('|')
+            print(f'{int(datos[0]):<15}{datos[1]:>10}{datos[2]:>10}')
+
+        f.close
+
+    def consultar_por_id(self, id_CTV):
+        f = open("c:/Archivos/Clonados/EQUIPO_3/clase_tema_video.txt",encoding="utf8")
+
+        print(f"{'IDCTV':<10}{'IDCT':>10}{'IDVIDEO':<10}")
+        for linea in f:
+            datos = linea.strip().split('|')
+            if int(datos[0]) == id_CTV:
+                print(f'{int(datos[0]):<15}{datos[1]:>10}{datos[2]:>10}')
+
+        f.close
