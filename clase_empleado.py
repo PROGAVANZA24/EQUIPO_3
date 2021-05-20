afcedfee2cf13ecb99f7520fd81e462e87286952
Mauricio Angel Olvera Chapa <angel.olverachp@uanl.edu.mx>
2021-05-19 19:51:44 -0500

    Se creo la clase Empleado en clase_empleado.py

---
+++ clase_empleado.py
@@ -0,0 +1,33 @@
+class Empleado:
+    def __init__(self,id_empleado,nombre,direccion):
+        self.__id_empleado = id_empleado
+        self.__nombre = nombre
+        self.__direccion = direccion
+    
+    def guardar(self,id_empleado,nombre,direccion):
+        lista = [id_empleado,"|",nombre,"|",direccion]
+        f = open("c:/Archivos/Clonados/EQUIPO_3/Empleados.txt","a",enconding="utf8")
+        for numero in lista:
+             f.write(str(numero)+'\n')
+        f.close
+
+    def consultar_todo(self):
+        f = open("c:/Archivos/Clonados/EQUIPO_3/Empleados.txt",encoding="utf8")
+
+        print(f"{'ID Empleado':<10}{'Nombre':>10}{'Direccion':>10}")
+        for linea in f:
+            datos = linea.strip().split('|')
+            print(f'{int(datos[0]):<15}{datos[1]:>10}{datos[2]:>10}')
+
+        f.close
+
+    def consultar_por_id(self,id_empleado):
+        f = open("c:/Archivos/Clonados/EQUIPO_3/Empleados.txt",encoding="utf8")
+
+        print(f"{'ID Empleado':<10}{'Nombre':>10}{'Direccion':>10}")
+        for linea in f:
+            datos = linea.strip().split('|')
+            if int(datos[0]) == id_empleado:
+                print(f'{int(datos[0]):<15}{datos[1]:>10}{datos[2]:>10}')
+
+        f.close
