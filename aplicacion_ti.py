0d0fee0b2bcdff0d5e69e060165119b061737aa3
Mauricio Angel Olvera Chapa <angel.olverachp@uanl.edu.mx>
2021-05-19 20:40:04 -0500

    Se agregaron ccinco lineas

--- aplicacion_ti.py
+++ aplicacion_ti.py
@@ -3,3 +3,8 @@ from Clase_Video import Clase_Video
 from clase_tema_video import Curso_Tema_Video
 from Curso_Tema import Curso_Tema
 from clase_curso import Curso
+from clase_empleado import Empleado
+
+print("------------------------------")
+print("Bienvenido al Menu Principal!")
+print("Seleccione la opcion deseada.")
